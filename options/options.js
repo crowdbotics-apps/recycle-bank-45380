a5491f4ff3019f36f7df28f083b290bb7f427b7a
Crowdbotics <team@crowdbotics.com>
2024-01-14 11:03:22 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "recycle-bank-45380",
+  url: "https://recycle-bank-45380.botics.co",
+  api: "https://recycle-bank-45380.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
